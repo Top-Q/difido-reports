12706d449c41d1639b55799eca4b18443565fe92
itaiag <itai.agmon@gmail.com>
2015-05-12 21:10:42 +0300

    Changing the sleep to of the execution ender from 10 seconds to 30

--- difido-server/difido-report-server/src/main/java/il/co/topq/report/model/ExecutionEnder.java
+++ difido-server/difido-report-server/src/main/java/il/co/topq/report/model/ExecutionEnder.java
@@ -16,7 +16,7 @@ public class ExecutionEnder implements ResourceChangedListener {
 
 	private final Logger log = LoggerFactory.getLogger(ExecutionEnder.class);
 
-	protected static final long THREAD_SLEEP_TIME = 10000;
+	protected static final long THREAD_SLEEP_TIME = 30000;
 
 	private Thread executionChecker;
 
@@ -36,7 +36,7 @@ public class ExecutionEnder implements ResourceChangedListener {
 						} catch (InterruptedException e) {
 							// Not going to happen
 						}
-						log.debug("Waking up to search for executions that needs to be ended");
+						log.debug("Waking up in order to search for executions that need to end");
 						final ExecutionMetaData[] metaDataArr = ExecutionManager.INSTANCE.getAllMetaData();
 						for (ExecutionMetaData meta : metaDataArr) {
 							if (!meta.isActive()) {
