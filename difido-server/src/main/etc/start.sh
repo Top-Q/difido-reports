b0bf2a7fbf4e7cb475c8653a6a5cc779f246dcc2
itaiag <itai.agmon@gmail.com>
2016-02-14 14:44:39 +0200

    Changing the log behaviour a bit

--- difido-server/src/main/etc/start.sh
+++ difido-server/src/main/etc/start.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
 
+
+#=======================================
+#      SPRING BOOT CONFIGURATION
+#=======================================
+
 # The port to use
 readonly PORT=8080
 
-# Host name. 
 readonly HOST=0.0.0.0
+
+# Log levels: TRACE, DEBUG, INFO, WARN, ERROR
+readonly LOGGING_LEVEL=ERROR
+
+
+
+#=========================================
+#      DEBUG                          
+#=========================================
+
+DEBUG=""
+# Remove the remark from the following line if you want to remote debug the server
+#DEBUG="-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8787,server=y,suspend=n"
+
+
+#=========================================
+#     LAUNCH
+#=========================================
 
 # Set the working directory to the parent folder of the script
 cd "$(dirname "$0")"/..
 
 # Launching the server
-java -jar -Dserver.address=$HOST -Dserver.port=$PORT lib/difido-server.jar
+java -jar $DEBUG -Xms256m -Xmx1024m -Dserver.address=$HOST -Dserver.port=$PORT -Dlogging.level.org.springframework.web=$LOGGING_LEVEL lib/difido-server.jar
