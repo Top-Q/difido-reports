50e39dc1b8573b6655197e1edc393044251eedaa
ITAI <ITAI>
2016-05-26 23:43:58 +0300

    Support for plugins in Windows

--- difido-server/src/main/etc/start.bat
+++ difido-server/src/main/etc/start.bat
@@ -11,4 +11,4 @@ set PORT=8080
 set LOGGING_LEVEL=WARN
 rem set DEBUG=-agentlib:jdwp=transport=dt_socket,server=y,address=8000,suspend=n
 
-"%_JAVACMD%" %DEBUG% -Xms256m -Xmx1024m -Dserver.address=%HOST% -Dserver.port=%PORT% -Dlogging.level.org.springframework.web=ERROR -Dlogging.level.il.co.topq.report=%LOGGING_LEVEL% -cp "lib\difido-server.jar" org.springframework.boot.loader.JarLauncher
+"%_JAVACMD%" %DEBUG% -Xms256m -Xmx1024m -Dserver.address=%HOST% -Dserver.port=%PORT% -Dlogging.level.org.springframework.web=ERROR -Dlogging.level.il.co.topq.report=%LOGGING_LEVEL% -cp "lib\difido-server.jar;plugin\*" org.springframework.boot.loader.JarLauncher
