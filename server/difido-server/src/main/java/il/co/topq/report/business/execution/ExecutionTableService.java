4654d0ccb22b8b180ab72a6477e077d3c864d7d0
anhermon <angel_mail@gmx.com>
2016-08-16 11:03:08 +0300

    -Add configuration field for headers. (#92)
    * -Add configuration field for headers.
    -Use headers configuration field when populating DataTable.
    -Move DataTable population logic out of the rest controller into its own
    service.
    
    * fix nullPointerException when metadata is empty
    
    * Fix another nullPointerException
    
    issue #91

---
+++ server/difido-server/src/main/java/il/co/topq/report/business/execution/ExecutionTableService.java
@@ -0,0 +1,121 @@
+package il.co.topq.report.business.execution;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import il.co.topq.report.Configuration;
+import il.co.topq.report.Configuration.ConfigProps;
+import il.co.topq.report.front.rest.ReportsResource.DataTable;
+
+@Service
+public class ExecutionTableService {
+	
+	private static final String ID = "Id";
+	private static final String DESCRIPTION = "Description";
+	private static final String LINK = "Link";
+	private static final String DATE = "Date";
+	private static final String TIME = "Time";
+	private static final String NUM_OF_TESTS = "# Tests";
+	private static final String NUM_OF_SUCCESSFUL = "# Successful";
+	private static final String NUM_OF_WARNINGS = "# Warnings";
+	private static final String NUM_OF_FAILS = "# Failed";
+	private static final String NUM_OF_MACHINES = "# Machines";
+	private static final String ACTIVE = "Active";
+	private static final String LOCKED = "Locked";
+	private static final String[] DEFAULT_HEADERS = new String[]{ID, DESCRIPTION, LINK, DATE, TIME, NUM_OF_TESTS, NUM_OF_SUCCESSFUL,
+			NUM_OF_WARNINGS, NUM_OF_FAILS, NUM_OF_MACHINES, ACTIVE, LOCKED};
+
+	public DataTable initTable(ExecutionMetadata[] metaData) {
+		DataTable table = new DataTable();	
+		List<String> headers = Arrays.asList(Configuration.INSTANCE.readString(ConfigProps.HEADERS).split("\\s*,\\s"));
+		if (headers == null || headers.size() == 0) {
+			headers = Arrays.asList(DEFAULT_HEADERS);
+		}
+		for (String header : headers) {
+			table.headers.add(header.trim());
+		}
+		
+		for (ExecutionMetadata meta : metaData) {
+			final Map<String, Object> row = new HashMap<String, Object>();
+			List<String> executionHeaders = new LinkedList<>();
+			executionHeaders.addAll(headers);
+			if (meta != null && meta.getProperties() != null) {
+				executionHeaders.addAll(meta.getProperties().keySet());
+			}
+			for(String header : headers) {
+				populateRow(table, row, header, meta);
+			}
+			table.data.add(row);
+		}
+		return table;
+	}
+
+	private void populateRow(DataTable table, Map<String, Object> row, String header, ExecutionMetadata meta) {
+		if( ! table.headers.contains(header)) {
+			table.headers.add(header);
+		}
+		if (header.equalsIgnoreCase(ID)) {
+			  row.put(ID, meta.getId());
+			  return;
+		}
+		if (header.equalsIgnoreCase(DESCRIPTION)) {
+			if (meta.getDescription() != null && !meta.getDescription().isEmpty()){
+				row.put(DESCRIPTION, meta.getDescription());
+			} else {
+				row.put(DESCRIPTION, meta.getFolderName());
+			}
+			return;
+		}
+		if ( header.equalsIgnoreCase(LINK)) {
+			row.put(LINK, meta.getUri());
+			return;
+		}
+		if ( header.equalsIgnoreCase(DATE)) {
+			row.put(DATE, meta.getDate());
+			return;
+		}
+		if ( header.equalsIgnoreCase(TIME)) {
+			row.put(TIME, meta.getTime());
+			return;
+		}
+		if ( header.equalsIgnoreCase(NUM_OF_TESTS)) {
+			row.put(NUM_OF_TESTS, meta.getNumOfTests());
+			return;
+		}
+		if ( header.equalsIgnoreCase(NUM_OF_SUCCESSFUL)) {
+			row.put(NUM_OF_SUCCESSFUL, meta.getNumOfSuccessfulTests());
+			return;
+		}
+		if ( header.equalsIgnoreCase(NUM_OF_WARNINGS)) {
+			row.put(NUM_OF_WARNINGS, meta.getNumOfTestsWithWarnings());
+			return;
+		}
+		if ( header.equalsIgnoreCase(NUM_OF_FAILS)) {
+			row.put(NUM_OF_FAILS, meta.getNumOfFailedTests());
+			return;
+		}
+		if ( header.equalsIgnoreCase(NUM_OF_MACHINES)) {
+			row.put(NUM_OF_MACHINES, meta.getNumOfMachines());
+			return;
+		}
+		if ( header.equalsIgnoreCase(ACTIVE)) {
+			row.put(ACTIVE, meta.isActive());
+		    return;
+		}	
+		if ( header.equalsIgnoreCase(LOCKED)) {
+			row.put(LOCKED, meta.isLocked());
+			return;
+		}
+		if ( meta == null || meta.getProperties() == null) {
+			return;
+		}
+		String value = meta.getProperties().get(header);
+		row.put(header, value != null ? value : "");
+	}
+
+}
--- server/difido-server/src/main/java/il/co/topq/report/front/rest/ReportsResource.java
+++ server/difido-server/src/main/java/il/co/topq/report/front/rest/ReportsResource.java
@@ -1,7 +1,6 @@
 package il.co.topq.report.front.rest;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -18,25 +17,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
-import il.co.topq.report.business.execution.ExecutionMetadata;
+import il.co.topq.report.business.execution.ExecutionTableService;
 import il.co.topq.report.business.execution.MetadataProvider;
 
 @RestController
 @Path("api/reports")
 public class ReportsResource {
 
-	private static final String ID = "Id";
-	private static final String DESCRIPTION = "Description";
-	private static final String LINK = "Link";
-	private static final String DATE = "Date";
-	private static final String TIME = "Time";
-	private static final String NUM_OF_TESTS = "# Tests";
-	private static final String NUM_OF_SUCCESSFUL = "# Successful";
-	private static final String NUM_OF_WARNINGS = "# Warnings";
-	private static final String NUM_OF_FAILS = "# Failed";
-	private static final String NUM_OF_MACHINES = "# Machines";
-	private static final String ACTIVE = "Active";
-	private static final String LOCKED = "Locked";
+
 
 	private static final Logger log = LoggerFactory.getLogger(ExecutionResource.class);
 
@@ -46,6 +34,9 @@ public class ReportsResource {
 	public ReportsResource(MetadataProvider metadataProvider) {
 		this.metadataProvider = metadataProvider;
 	}
+	
+	@Autowired
+	public ExecutionTableService executionTableService;
 
 	/**
 	 * Get list of all the reports
@@ -57,52 +48,10 @@ public class ReportsResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public DataTable get(@PathParam("execution") int execution) {
 		log.debug("GET - Get all reports");
-		ExecutionMetadata[] metaData = metadataProvider.getAllMetaData();
-		final DataTable table = new DataTable();
-
-		table.headers.add(ID);
-		table.headers.add(DESCRIPTION);
-		table.headers.add(LINK);
-		table.headers.add(DATE);
-		table.headers.add(TIME);
-		table.headers.add(NUM_OF_TESTS);
-		table.headers.add(NUM_OF_SUCCESSFUL);
-		table.headers.add(NUM_OF_WARNINGS);
-		table.headers.add(NUM_OF_FAILS);
-		table.headers.add(NUM_OF_MACHINES);
-		table.headers.add(ACTIVE);
-		table.headers.add(LOCKED);
-
-		for (ExecutionMetadata meta : metaData) {
-			final Map<String, Object> row = new HashMap<String, Object>();
-			row.put(ID, meta.getId());
-			if (meta.getDescription() != null && !meta.getDescription().isEmpty()){
-				row.put(DESCRIPTION, meta.getDescription());
-			} else {
-				row.put(DESCRIPTION, meta.getFolderName());
-			}
-			row.put(LINK, meta.getUri());
-			row.put(DATE, meta.getDate());
-			row.put(TIME, meta.getTime());
-			row.put(NUM_OF_TESTS, meta.getNumOfTests());
-			row.put(NUM_OF_SUCCESSFUL, meta.getNumOfSuccessfulTests());
-			row.put(NUM_OF_WARNINGS, meta.getNumOfTestsWithWarnings());
-			row.put(NUM_OF_FAILS, meta.getNumOfFailedTests());
-			row.put(NUM_OF_MACHINES, meta.getNumOfMachines());
-			row.put(ACTIVE, meta.isActive());
-			row.put(LOCKED, meta.isLocked());
-			if (meta.getProperties() != null && meta.getProperties().size() > 0) {
-				for (String header : meta.getProperties().keySet()) {
-					table.headers.add(header);
-					row.put(header, meta.getProperties().get(header));
-				}
-			}
-			table.data.add(row);
-		}
-		return table;
+		return executionTableService.initTable(metadataProvider.getAllMetaData());
 	}
 
-	static class DataTable {
+	public static class DataTable {
 		// Holds the headers of the table. The data structure has to be ordered
 		// and not to allow duplications.
 		final public Set<String> headers = new LinkedHashSet<String>();
