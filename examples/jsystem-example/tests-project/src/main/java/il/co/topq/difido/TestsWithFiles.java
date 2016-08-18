d90ee881abd859d430de89fe789e0517e3bc8d6d
itaiag <itai.agmon@gmail.com>
2016-08-18 16:38:22 +0300

    Adding test example for sending files to JSystem

--- examples/jsystem-example/tests-project/src/main/java/il/co/topq/difido/TestsWithFiles.java
+++ examples/jsystem-example/tests-project/src/main/java/il/co/topq/difido/TestsWithFiles.java
@@ -6,9 +6,11 @@ import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
 import jsystem.framework.TestProperties;
@@ -18,10 +20,45 @@ import junit.framework.SystemTestCase4;
 public class TestsWithFiles extends SystemTestCase4 {
 
 	@Test
-	@TestProperties(name = "Adding the pom file to the report")
-	public void testAddFile() throws Exception {
-		File file = new File("pom.xml");
-		ReporterHelper.copyFileToReporterAndAddLink(report, file, "This is the POM file of the project");
+	@TestProperties(name = "Adding text file to the report and adding link")
+	public void testAddWithLinkFile() throws Exception {
+		File file = createFile();
+		try {
+			ReporterHelper.copyFileToReporterAndAddLink(report, file, "A general text file");
+		} finally {
+			file.delete();
+		}
+	}
+
+	@Test
+	@TestProperties(name = "Adding text file to the report and adding property")
+	public void testAddFileWithProperty() throws IOException {
+		File file = createFile();
+		try {
+			report.saveFile(file.getName(), FileUtils.readFileToByteArray(file));
+			report.addProperty("file", "<a href='" + file.getName() + "'>" + file.getName() + "</a>");
+		} finally {
+			file.delete();
+		}
+	}
+
+	private File createFile() throws IOException {
+		String content = "Lorem ipsum dolor sit amet, "
+				+ "consectetur adipiscing elit. Nunc id justo ac lacus finibus venenatis"
+				+ ". Nunc imperdiet nunc purus, non aliquam mi sagittis et. "
+				+ "Suspendisse metus erat, interdum at tincidunt vel, "
+				+ "euismod fermentum ipsum. Ut sapien arcu, tempus et ultricies a, "
+				+ "aliquet id elit. Phasellus et libero non lorem auctor malesuada "
+				+ "feugiat sed lectus. Morbi faucibus scelerisque felis, scelerisque volutpat risus "
+				+ "dapibus ac. Morbi nec sem aliquet, feugiat ex id, porttitor purus. "
+				+ "Duis mattis volutpat orci quis lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. "
+				+ "Morbi eu ex felis. Donec semper interdum eros, sed varius dolor finibus in. "
+				+ "Curabitur neque est, iaculis non quam in, posuere pellentesque nunc. "
+				+ "Mauris purus diam, pharetra et justo eget, congue tristique purus. "
+				+ "In vulputate mi justo, in volutpat magna porttitor a.";
+		File tempFile = File.createTempFile("tempTestingFile", ".txt");
+		FileUtils.write(tempFile, content);
+		return tempFile;
 	}
 
 	@Test
