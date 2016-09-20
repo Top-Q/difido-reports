7b1a428eb2c3ee65f2f8157f448cad8d6f857b44
itaiag <itai.agmon@gmail.com>
2016-09-20 15:11:44 +0300

    Fixed the filtered columns

--- server/difido-server/src/main/java/il/co/topq/report/business/execution/ExecutionTableService.java
+++ server/difido-server/src/main/java/il/co/topq/report/business/execution/ExecutionTableService.java
@@ -1,5 +1,6 @@
 package il.co.topq.report.business.execution;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -14,7 +15,7 @@ import il.co.topq.report.front.rest.ReportsResource.DataTable;
 
 @Service
 public class ExecutionTableService {
-	
+
 	private static final String ID = "Id";
 	private static final String DESCRIPTION = "Description";
 	private static final String LINK = "Link";
@@ -27,28 +28,42 @@ public class ExecutionTableService {
 	private static final String NUM_OF_MACHINES = "# Machines";
 	private static final String ACTIVE = "Active";
 	private static final String LOCKED = "Locked";
-	private static final String[] DEFAULT_HEADERS = new String[]{ID, DESCRIPTION, LINK, DATE, TIME, NUM_OF_TESTS, NUM_OF_SUCCESSFUL,
-			NUM_OF_WARNINGS, NUM_OF_FAILS, NUM_OF_MACHINES, ACTIVE, LOCKED};
+	private static final String[] DEFAULT_HEADERS = new String[] { ID, DESCRIPTION, LINK, DATE, TIME, NUM_OF_TESTS,
+			NUM_OF_SUCCESSFUL, NUM_OF_WARNINGS, NUM_OF_FAILS, NUM_OF_MACHINES, ACTIVE, LOCKED };
 
 	public DataTable initTable(ExecutionMetadata[] metaData) {
-		DataTable table = new DataTable();	
-		String headersStr = Configuration.INSTANCE.readString(ConfigProps.HEADERS);
-		List<String> headers = Arrays.asList(headersStr.split(","));
-		if (headersStr == null || headersStr.length() == 0 || headers == null || headers.size() == 0) {
-			headers = Arrays.asList(DEFAULT_HEADERS);
+		DataTable table = new DataTable();
+		List<String> headers;
+		if (!Configuration.INSTANCE.readString(ConfigProps.HEADERS).isEmpty()) {
+			headers = new ArrayList<String>();
+			// There are some columns that we always want to display. And also,
+			// it is really hard to to handle the 'description' column when it
+			// is too dynamic in the client side.
+			headers.add(ID);
+			headers.add(DESCRIPTION);
+			headers.add(LINK);
+			for (String desiredHeader: Configuration.INSTANCE.readString(ConfigProps.HEADERS).split(",")){
+				desiredHeader = desiredHeader.trim();
+				if (desiredHeader.equals(ID) || desiredHeader.equals(DESCRIPTION) || desiredHeader.equals(LINK)){
+					// We already added those headers.
+					continue;
+				}
+				headers.add(desiredHeader);
+			}
+		} else {
+			headers = new LinkedList<String>(Arrays.asList(DEFAULT_HEADERS));
+			for (ExecutionMetadata meta : metaData) {
+				headers.addAll(meta.getProperties().keySet());
+			}
 		}
+
 		for (String header : headers) {
 			table.headers.add(header.trim());
 		}
-		
+
 		for (ExecutionMetadata meta : metaData) {
 			final Map<String, Object> row = new HashMap<String, Object>();
-			List<String> executionHeaders = new LinkedList<>();
-			executionHeaders.addAll(headers);
-			if (meta != null && meta.getProperties() != null) {
-				executionHeaders.addAll(meta.getProperties().keySet());
-			}
-			for(String header : executionHeaders) {
+			for (String header : headers) {
 				populateRow(table, row, header, meta);
 			}
 			table.data.add(row);
@@ -57,62 +72,62 @@ public class ExecutionTableService {
 	}
 
 	private void populateRow(DataTable table, Map<String, Object> row, String header, ExecutionMetadata meta) {
-		if( ! table.headers.contains(header)) {
+		if (!table.headers.contains(header)) {
 			table.headers.add(header);
 		}
 		if (header.equalsIgnoreCase(ID)) {
-			  row.put(ID, meta.getId());
-			  return;
+			row.put(ID, meta.getId());
+			return;
 		}
 		if (header.equalsIgnoreCase(DESCRIPTION)) {
-			if (meta.getDescription() != null && !meta.getDescription().isEmpty()){
+			if (meta.getDescription() != null && !meta.getDescription().isEmpty()) {
 				row.put(DESCRIPTION, meta.getDescription());
 			} else {
 				row.put(DESCRIPTION, meta.getFolderName());
 			}
 			return;
 		}
-		if ( header.equalsIgnoreCase(LINK)) {
+		if (header.equalsIgnoreCase(LINK)) {
 			row.put(LINK, meta.getUri());
 			return;
 		}
-		if ( header.equalsIgnoreCase(DATE)) {
+		if (header.equalsIgnoreCase(DATE)) {
 			row.put(DATE, meta.getDate());
 			return;
 		}
-		if ( header.equalsIgnoreCase(TIME)) {
+		if (header.equalsIgnoreCase(TIME)) {
 			row.put(TIME, meta.getTime());
 			return;
 		}
-		if ( header.equalsIgnoreCase(NUM_OF_TESTS)) {
+		if (header.equalsIgnoreCase(NUM_OF_TESTS)) {
 			row.put(NUM_OF_TESTS, meta.getNumOfTests());
 			return;
 		}
-		if ( header.equalsIgnoreCase(NUM_OF_SUCCESSFUL)) {
+		if (header.equalsIgnoreCase(NUM_OF_SUCCESSFUL)) {
 			row.put(NUM_OF_SUCCESSFUL, meta.getNumOfSuccessfulTests());
 			return;
 		}
-		if ( header.equalsIgnoreCase(NUM_OF_WARNINGS)) {
+		if (header.equalsIgnoreCase(NUM_OF_WARNINGS)) {
 			row.put(NUM_OF_WARNINGS, meta.getNumOfTestsWithWarnings());
 			return;
 		}
-		if ( header.equalsIgnoreCase(NUM_OF_FAILS)) {
+		if (header.equalsIgnoreCase(NUM_OF_FAILS)) {
 			row.put(NUM_OF_FAILS, meta.getNumOfFailedTests());
 			return;
 		}
-		if ( header.equalsIgnoreCase(NUM_OF_MACHINES)) {
+		if (header.equalsIgnoreCase(NUM_OF_MACHINES)) {
 			row.put(NUM_OF_MACHINES, meta.getNumOfMachines());
 			return;
 		}
-		if ( header.equalsIgnoreCase(ACTIVE)) {
+		if (header.equalsIgnoreCase(ACTIVE)) {
 			row.put(ACTIVE, meta.isActive());
-		    return;
-		}	
-		if ( header.equalsIgnoreCase(LOCKED)) {
+			return;
+		}
+		if (header.equalsIgnoreCase(LOCKED)) {
 			row.put(LOCKED, meta.isLocked());
 			return;
 		}
-		if ( meta == null || meta.getProperties() == null) {
+		if (meta == null || meta.getProperties() == null) {
 			return;
 		}
 		String value = meta.getProperties().get(header);
