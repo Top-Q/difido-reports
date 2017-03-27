f0115e0d905df6ff2298b231a7faeb0a7d38953d
itaiag <itai.agmon@gmail.com>
2017-03-27 10:44:02 +0300

    Adding external links to the navigation bar. issue #123

---
+++ server/difido-server/docRoot/js/common.js
@@ -0,0 +1,49 @@
+
+function getLinksAndNames() {
+    return new Promise(function (fulfill, reject) {
+        $.ajax({
+            url: '/api/settings',
+            type: 'GET',
+        }).done(function (settings) {
+            var regex = /external.links:(.+)/;
+            if (regex.test(settings)) {
+                var values = regex.exec(settings)[1].trim().split(";");
+                var links = {};
+                if (values.length == 0) {
+                    reject()
+                }
+                values.forEach(function (value) {
+                    links[value.split("=")[0]] = value.split("=")[1];
+                });
+                fulfill(links);
+            } else {
+                reject();
+            }
+        });
+
+    });
+
+}
+
+
+
+function populateNavBar() {    
+    return new Promise(function (fullfill,reject) {
+        getLinksAndNames()
+            .then(function (links) {
+                if ("undefined" === typeof links) {
+                    reject();
+                }
+                var nav = $('#navbar');
+                for (name in links) {
+                    var option = $("<li>").attr("id", name).append($("<a>").text(name).attr("href", "generic.html?name=" + name + "&link=" + links[name]));
+                    nav.append(option);
+                }
+                fullfill();
+            })
+            .catch(e => console.log("No external links were defined"));
+
+    });
+
+
+}
