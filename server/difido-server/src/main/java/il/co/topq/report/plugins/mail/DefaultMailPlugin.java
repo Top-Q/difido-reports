c081817545e52ba33cd054f4786dae11f9b6d04b
metialmi <mmeitalm@gmail.com>
2017-08-14 11:35:19 +0300

    Adding the ability to add attachments via the mail plugin

--- server/difido-server/src/main/java/il/co/topq/report/plugins/mail/DefaultMailPlugin.java
+++ server/difido-server/src/main/java/il/co/topq/report/plugins/mail/DefaultMailPlugin.java
@@ -202,6 +202,11 @@ public class DefaultMailPlugin implements ExecutionPlugin {
 		if (!StringUtils.isEmpty(cc)) {
 			sender.setSendCc(cc.split(";"));
 		}
+		
+		final String[] attachments = getAttachments();
+		if (null != attachments && attachments.length != 0) {
+			sender.setAttachments(attachments);
+		}
 	}
 
 	protected String getCcAddresses() {
@@ -214,6 +219,10 @@ public class DefaultMailPlugin implements ExecutionPlugin {
 
 	protected String getFromAddress() {
 		return Configuration.INSTANCE.readString(ConfigProps.MAIL_FROM_ADDRESS);
+	}
+	
+	protected String[] getAttachments() {
+		return new String[]{};
 	}
 
 	protected boolean isEnabled() {
