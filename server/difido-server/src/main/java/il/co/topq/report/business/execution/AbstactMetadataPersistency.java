10494554ea3f4fbc1934fe79e4aa326550cf3d52
itaiag <itai.agmon@gmail.com>
2018-01-10 21:43:29 +0200

    Fixing memory leak by removing old execution objets. issue #181

--- server/difido-server/src/main/java/il/co/topq/report/business/execution/AbstactMetadataPersistency.java
+++ server/difido-server/src/main/java/il/co/topq/report/business/execution/AbstactMetadataPersistency.java
@@ -8,10 +8,15 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import il.co.topq.report.StopWatch;
 
+@Component
 public abstract class AbstactMetadataPersistency implements MetadataPersistency {
+
+	private static final long MAX_TIME_TO_KEEP_EXECUTION_IN_SECONDS = 60 * 10;
 
 	private final Logger logger;
 
@@ -52,6 +57,40 @@ public abstract class AbstactMetadataPersistency implements MetadataPersistency 
 		return ++lastId;
 	}
 
+	/**
+	 * Will release execution object from metadata objects that have ended more
+	 * then <code>MAX_TIME_TO_KEEP_EXECUTION_IN_SECONDS</code> seconds ago.<br>
+	 * The reason we can't simply do it when execution is over is that there may
+	 * be plugin and controllers that are using the execution object when after
+	 * the execution is over. From the other hand, we can't keep them in memory
+	 * since it is a memory leak.<br>
+	 * This is what you get when you design stateful system
+	 * issue #181
+	 * 
+	 */
+	@Scheduled(fixedRate = 1000 * 60 * 10)
+	private void scheduledReleaseOfExecutionObjects() {
+		logger.debug("About to check if there is a need to free the execution object from executions");
+		StopWatch stopWatch = new StopWatch(logger).start("Checking if there is a need to free execution objects");
+		// @formatter:off
+		final long currentTime = System.currentTimeMillis();
+		executionsCache
+			.values()
+			.stream()
+			// filter for all the executions without execution object
+			.filter(metadata -> null != metadata.getExecution())
+			// We will clean only execution objects of executions that are no longer active
+			.filter(metadata -> !metadata.isActive())
+			// And only if the last access time is longer then the maximum specified
+			.filter(metadata -> (currentTime - metadata.getLastAccessedTime()) / 60 >= MAX_TIME_TO_KEEP_EXECUTION_IN_SECONDS )
+			.forEach(metadata -> {
+				logger.debug("Releasing execution object from execution " + metadata.getId());
+				metadata.setExecution(null);
+			});
+		// @formatter:on
+		stopWatch.stopAndLog();
+	}
+
 	@Override
 	public synchronized void add(ExecutionMetadata metadata) {
 		readFromPersistency();
@@ -73,7 +112,7 @@ public abstract class AbstactMetadataPersistency implements MetadataPersistency 
 		StopWatch stopWatch = new StopWatch(logger).start("Writing to persistency");
 		writeToPersistency();
 		stopWatch.stopAndLog();
-		
+
 	}
 
 	@Override
