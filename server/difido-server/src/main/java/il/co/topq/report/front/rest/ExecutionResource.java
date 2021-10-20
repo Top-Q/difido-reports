3e2d9229de6c510448eed24b242546ec071cfaf5
eduardpo <eduard.p@siklu.com>
2021-10-20 15:03:11 +0300

    empty string to serial column before update

--- server/difido-server/src/main/java/il/co/topq/report/front/rest/ExecutionResource.java
+++ server/difido-server/src/main/java/il/co/topq/report/front/rest/ExecutionResource.java
@@ -201,7 +201,7 @@ public class ExecutionResource {
 		metaData.setComment("");
 		if (executionDetails != null) {
 			metaData.setDescription(executionDetails.getDescription());
-			metaData.setSerialNum(""); // emty string to serial column before update
+			metaData.setSerialNum(""); // empty string to serial column before update
 			metaData.setShared(executionDetails.isShared());
 			setAllowedPropertiesToMetaData(metaData, executionDetails);
 		}
